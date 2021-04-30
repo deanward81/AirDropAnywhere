0748a21f273c65b5819a0fec605e805c07a30fb4
Dean Ward <dean.ward@bakedbean.org.uk>
2021-04-30 13:41:56 +0100

    Initial project structure

---
+++ src/AirDropAnywhere.Cli/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AirDropAnywhere.Cli
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+}
